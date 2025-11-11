1ad875fca3d547db07e78293a8da980d064ab1fa
shadykip <shady.kip@outlook.com>
2025-11-11 15:39:45 +0300

    Ready for Railway deploy

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net/url"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -12,20 +14,31 @@ import (
 )
 
 func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "host=localhost user=dev password=linspace dbname=taskflow port=5432 sslmode=disable"
+	}
 	// DB setup (local fallback)
-	dsn := "host=localhost user=dev password=linspace dbname=taskflow port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		panic("DB connection failed")
 	}
 	db.AutoMigrate(&models.URL{})
 
 	// 🧠 Redis setup (local fallback)
-	redisAddr := os.Getenv("REDIS_ADDR")
-	if redisAddr == "" {
-		redisAddr = "localhost:6379" // Docker/local Redis
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		redisURL = "localhost:6379"
+	} else {
+		// Railway Redis URL is like: redis://user:pass@host:port
+		// Parse host:port
+		u, err := url.Parse(redisURL)
+		if err != nil {
+			log.Fatal("Invalid REDIS_URL")
+		}
+		redisURL = u.Host // e.g., "host:port"
 	}
-	redisCache := cache.NewRedisCache(redisAddr)
+	redisCache := cache.NewRedisCache(redisURL)
 
 	r := gin.Default()
 	r.POST("/shorten", handlers.ShortenHandler(db))
